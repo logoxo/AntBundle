8fe2243ec7d16a3d81253a599a0310416fcb7ac7
Noko Lautze <insideclass99@gmail.com>
2022-04-27 22:04:32 +0200

    removed some taps

--- src/scripts/metamask.js
+++ src/scripts/metamask.js
@@ -55,44 +55,44 @@ export default {
 
   },
   async setup(currentNetworkId){
-        let that = this;
-        console.log("================ metamask") 
-        // https://ethereum.stackexchange.com/questions/93502/metamask-api-cant-detect-events-connect-and-disconnect-in-react-js 
-        // https://ethereum.stackexchange.com/questions/42768/how-can-i-detect-change-in-account-in-metamask
-        //  check if user hass been locked out from metamask
-        window.ethereum.on("accountsChanged", async function(accounts) {
-           
-          if(accounts.length === 0){
-            //console.log("have been logout") 
-            store.dispatch("logout"); 
-          }else{
-            store.dispatch("send", accounts[0]); 
-            store.dispatch("login");  
-          }
+    let that = this;
+    console.log("================ metamask") 
+    // https://ethereum.stackexchange.com/questions/93502/metamask-api-cant-detect-events-connect-and-disconnect-in-react-js 
+    // https://ethereum.stackexchange.com/questions/42768/how-can-i-detect-change-in-account-in-metamask
+    //  check if user hass been locked out from metamask
+    window.ethereum.on("accountsChanged", async function(accounts) {
+       
+      if(accounts.length === 0){
+        //console.log("have been logout") 
+        store.dispatch("logout"); 
+      }else{
+        store.dispatch("send", accounts[0]); 
+        store.dispatch("login");  
+      }
 
-        });
+    });
 
-        // check if user have been changed the metamask network
-        window.ethereum.on('chainChanged', function () {
-            const networkId = ct.networkId;
-            if(networkId !== currentNetworkId){
-              window.ethereum.request({
-              method: 'wallet_switchEthereumChain',
-                params: [{ chainId: w3.prov.utils.toHex(networkId) }],
-              });   
-            }
-        })
-
-        const accounts  = await window.ethereum.request({ method: "eth_accounts" });
-
-        // checks if account exists  
-        if(accounts.length !== 0 ){
-         const accounts  = await window.ethereum.request({
-           method: "eth_accounts"
-          })
-          store.dispatch("send", accounts[0]);
-          store.dispatch("login");  
-          that.$forceUpdate;
+    // check if user have been changed the metamask network
+    window.ethereum.on('chainChanged', function () {
+        const networkId = ct.networkId;
+        if(networkId !== currentNetworkId){
+          window.ethereum.request({
+          method: 'wallet_switchEthereumChain',
+            params: [{ chainId: w3.prov.utils.toHex(networkId) }],
+          });   
         }
+    })
+
+    const accounts  = await window.ethereum.request({ method: "eth_accounts" });
+
+    // checks if account exists  
+    if(accounts.length !== 0 ){
+     const accounts  = await window.ethereum.request({
+       method: "eth_accounts"
+      })
+      store.dispatch("send", accounts[0]);
+      store.dispatch("login");  
+      that.$forceUpdate;
+    }
   }
 }
