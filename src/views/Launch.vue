5da23df2729efe0afd8c8c32cbaa957ae994c442
Noko Lautze <insideclass99@gmail.com>
2022-04-27 22:16:21 +0200

    changed Launch design

--- src/views/Launch.vue
+++ src/views/Launch.vue
@@ -1,21 +1,21 @@
 <template>
-  <div  :style="{ backgroundImage: 'url(' + this.imgUrl('background1') + ')' }" 
-    class="bg-color-wrapper bg-cover h-screen flex items-center justify-center ">
-
+  <div  :style="{ backgroundImage: 'url(' + this.imgUrl('lanchBackground') + ')' }" 
+    class="bg-color-wrapper bg-cover h-screen  ">
+    <div class="bg-black-layer w-full h-screen flex items-center justify-center">
     <!-- Card -->
-    <div class="bg-white border-2 border-gray-300 rounded-2xl p-5 w-96">
+    <div class="bg-white border-2 border-amber-200 rounded-2xl p-5 w-96">
     
       <!-- Caption -->
-      <p class="text-2xl font-bold text-gray-700 pt-4 text-center">Connect to AntBundle and become an Ant.</p>
+      <p class="text-2xl font-bold text-gray-500 pt-4 text-center">Connect to AntBundle and become an Ant.</p>
 
       <!-- Register Button -->
       <button 
-        class="hover:bg-blue-600 bg-gray-600 delay-75 
+        class="hover:bg-amber-600 bg-amber-500 delay-75 
         duration-100 text-white text-sm font-bold rounded-2xl 
         w-full py-3 mt-7 border-b-4  border-b-green-600"
         @click="lanch()" >Connect with Metamask</button> 
     </div>
-
+    </div>
   </div>
 </template>
 
